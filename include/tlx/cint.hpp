efef27b3a84141547d96d4da4db93ac0d946028c
Muhammet Kaya <muham123cak@gmail.com>
2026-08-08 22:19:44 +0300

    feat: added names for types

---
+++ include/tlx/cint.hpp
@@ -0,0 +1,32 @@
+//
+// Created by muham on 8.08.2026.
+//
+
+#ifndef TLX_CINT_HPP
+#define TLX_CINT_HPP
+
+namespace tlx {
+    using u0        = void;
+    using void_t    = void;
+
+    using boolean   = bool;
+
+    using float32_t = float;
+    using float64_t = double;
+
+    using char_t    = char;
+
+    using size_t    = unsigned long long;
+
+    using int8_t    = signed char;
+    using int16_t   = signed short;
+    using int32_t   = signed int;
+    using int64_t   = signed long long;
+
+    using uint8_t   = unsigned char;
+    using uint16_t  = unsigned short;
+    using uint32_t  = unsigned int;
+    using uint64_t  = unsigned long long;
+} //namespace tlx
+
+#endif //TLX_CINT_HPP
